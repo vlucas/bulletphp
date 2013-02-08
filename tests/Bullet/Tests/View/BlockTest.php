94bffb7a811b18070d4edd5a9134293dbb4b2490
Vance Lucas <vance@vancelucas.com>
2013-02-07 21:15:32 -0600

    More tests for all block methods

--- tests/Bullet/Tests/View/BlockTest.php
+++ tests/Bullet/Tests/View/BlockTest.php
@@ -24,6 +24,61 @@ class BlockTest extends \PHPUnit_Framework_TestCase
         Template::config($this->oldConfig);
     }
 
+    public function testBlockDefaultContent()
+    {
+        $tpl = new Template('block');
+        $block = $tpl->block(__FUNCTION__, function() { echo "Default Content"; });
+        $this->assertEquals("Default Content", $block->content());
+    }
+
+    public function testBlockAppend()
+    {
+        $tpl = new Template('block');
+        $block = $tpl->block(__FUNCTION__)->append(function() { echo "Test"; });
+        $this->assertEquals("Test", $block->content());
+    }
+
+    public function testBlockAppendAppendsContent()
+    {
+        $tpl = new Template('block');
+        $block = $tpl->block(__FUNCTION__)->content(function() { echo 'Content'; })
+            ->append(function() { echo 'After'; });
+        $this->assertEquals("ContentAfter", $block->content());
+    }
+
+    public function testBlockAppendTemplateRender()
+    {
+        $tpl = new Template('test');
+        $tpl->layout('layouts/block');
+        $block = $tpl->block('js')->content(function() { })
+            ->append(function() { echo 'Content'; });
+        $this->assertEquals("Content<div><p>Test</p></div>", $tpl->content());
+    }
+
+    public function testBlockPreppend()
+    {
+        $tpl = new Template('block');
+        $block = $tpl->block(__FUNCTION__)->prepend(function() { echo "Test"; });
+        $this->assertEquals("Test", $block->content());
+    }
+
+    public function testBlockPrependPrependsContent()
+    {
+        $tpl = new Template('block');
+        $block = $tpl->block(__FUNCTION__)->content(function() { echo 'Content'; })
+            ->prepend(function() { echo 'Before'; });
+        $this->assertEquals("BeforeContent", $block->content());
+    }
+
+    public function testBlockPrependTemplateRender()
+    {
+        $tpl = new Template('test');
+        $tpl->layout('layouts/block');
+        $block = $tpl->block('js')->content(function() { })
+            ->prepend(function() { echo 'Content'; });
+        $this->assertEquals("Content<div><p>Test</p></div>", $tpl->content());
+    }
+
     public function testBlockRendersInLayoutWithSetContent()
     {
         $tpl = new Template('block');

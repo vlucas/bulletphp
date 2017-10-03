fea1f32a96b4d8f3fc901e7cb0a176fc56bf7180
Fábián Tamás László <giganetom@gmail.com>
2017-10-03 20:55:16 +0200

    Adds \Bullet\Response\Chunked & tests

---
+++ src/Bullet/Response/Chunked.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace Bullet\Response;
+
+/**
+ * Chunked Response Class
+ *
+ * Extends the Bullet Response class to be able to handle iterable
+ * (list, or array-like) $content. Such content will be sent with the
+ * HTTP chunked encoding.
+ * 
+ * @package Bullet\Response
+ * @author Fábián Tamás László <giganetom@gmail.com>
+ * @license http://www.opensource.org/licenses/mit-license.php
+ */
+
+class Chunked extends \Bullet\Response
+{
+    /**
+     * Maximal size of the emitted chunks.
+     * 
+     * Can be set to an arbitrary non-negative integer value.
+     * 
+     * Zero means that every element of the content will be sent as a whole
+     * chunk.
+     */
+    public $chunkSize = 4096;
+    protected $_items;
+
+    public function __construct($items, $status = 200) {
+        parent::__construct('', $status);
+        $this->_items = $items;
+    }
+
+    protected function _sendChunk($buf) {
+        printf("%x\r\n%s\r\n", strlen($buf), $buf);
+        flush(); // HHVM doesn't do implicit flush, it eats all memory instead. Nice.
+    }
+
+    /**
+     * Emits whole chunks and remove them from a buffer.
+     */
+    protected function _sendChunks(&$buf, $chunkSize) {
+        $chunks = str_split($buf, $chunkSize);
+
+        if (strlen(end($chunks)) != $chunkSize) {
+            $buf = array_pop($chunks);
+        } else {
+            $buf = '';
+        }
+
+        foreach ($chunks as $chunk) {
+            $this->_sendChunk($chunk);
+        }
+    }
+
+    public function send() {
+        // Write and close session
+        if(session_id()) {
+            session_write_close();
+        }
+
+        if (!headers_sent()) {
+            $this->header('Transfer-Encoding', 'chunked');
+            $this->header('Content-Encoding', 'none');
+
+            $this->sendStatus();
+            $this->sendHeaders();
+        }
+
+        if ($this->chunkSize <= 0) {
+            foreach ($this->_items as $chunk) {
+                $this->_sendChunk((string) $chunk); // Just send chunks as they are, no need to buffer around.
+            }
+            // Emit a zero-length closing chunk
+            $this->_sendChunk('');
+        } else {
+            // Everyday I'm buffering...
+            $buf = '';
+            foreach ($this->_items as $chunk) {
+                $buf .= (string) $chunk; // Grow the buffer
+                $this->_sendChunks($buf, $this->chunkSize); // Emit whole chunks. Might leave a partial chunk behind in $buf.
+            }
+            // The remaining buffer might still have a partial chunk, send it.
+            if (strlen($buf) > 0) {
+                $this->_sendChunk($buf);
+            }
+            $this->_sendChunk('');
+        }
+    }
+
+    /**
+     * This is bad, such response cannot be converted to
+     * a string.
+     * 
+     * Always return empty string, but use send() as a side-effect.
+     */
+    public function __toString()
+    {
+        trigger_error("Chunked response is converted to string. This does not make much sense. Use send() instead.");
+        $this->send();
+        return "";
+    }
+
+}
---
+++ tests/Bullet/Tests/ChunkedResponseTest.php
@@ -0,0 +1,62 @@
+<?php
+namespace Bullet\Tests;
+
+class ChunkedTest extends \PHPUnit_Framework_TestCase
+{
+    private $_testContent = [
+        "The", " ", "quick", " brown", " fox ", "jumps", " over ", "the ",
+        "lazy ", "dog."
+    ];
+    private function _runTestBulletApp($chunkSize, $content) {
+        $app = new \Bullet\App();
+        $app->path('/test', function($request) use ($chunkSize, $content) {
+            $c = new \Bullet\Response\Chunked($content);
+            $c->chunkSize = $chunkSize;
+            return $c;
+        });
+        $response = $app->run('GET', '/test');
+        $this->assertInstanceOf('\Bullet\\Response\\Chunked', $response);
+        ob_start();
+        $response->send();
+        $output = ob_get_clean();
+        return $output;
+    }
+    public function testNonBufferedChunkedEncoding() {
+        $output = $this->_runTestBulletApp(0, $this->_testContent);
+        $shouldBe = '';
+        foreach ($this->_testContent as $word) {
+            $shouldBe .= sprintf("%x\r\n%s\r\n", strlen($word), $word);
+        }
+        $shouldBe .= "0\r\n\r\n";
+        $this->assertEquals($shouldBe, $output);
+    }
+    public function testBufferedChunkedEncoding() {
+        $CHUNKSIZE = 10;
+        $output = $this->_runTestBulletApp($CHUNKSIZE, $this->_testContent);
+        $shouldBe = '';
+        foreach (str_split(implode('', $this->_testContent), $CHUNKSIZE) as $word) {
+            $shouldBe .= sprintf("%x\r\n%s\r\n", strlen($word), $word);
+        }
+        $shouldBe .= "0\r\n\r\n";
+        $this->assertEquals($shouldBe, $output);
+    }
+    public function testGeneratorFunction() {
+        $CHUNKSIZE = 10;
+        $content = function () {
+            for ($i = 0; $i < 5; $i++) {
+                yield "xxxxx";
+            }
+        };
+        $output = $this->_runTestBulletApp(10, $content());
+        $strcontent = '';
+        foreach ($content() as $piece) {
+            $strcontent .= $piece;
+        }
+        $shouldBe = '';
+        foreach (str_split($strcontent, $CHUNKSIZE) as $word) {
+            $shouldBe .= sprintf("%x\r\n%s\r\n", strlen($word), $word);
+        }
+        $shouldBe .= "0\r\n\r\n";
+        $this->assertEquals($shouldBe, $output);
+    }
+}
